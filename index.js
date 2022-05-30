9620b19f0e36984f1edebfa874dd38b96e15f296
Brahm <brahm.kelly@gmail.com>
2022-05-30 10:54:56 -0400

    setup for aws api

---
+++ index.js
@@ -0,0 +1,53 @@
+const mongo = require("mongodb").MongoClient;
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const url =
+  "mongodb+srv://admin:Password1@cluster0.xks18.mongodb.net?retryWrites=true&w=majority";
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+let menudb, customersdb, db;
+
+mongo.connect(url, options, (err, mongoClient) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log("Connected!");
+  db = mongoClient.db("restaurant");
+  customersdb = db.collection("customers");
+  menudb = db.collection("menu");
+});
+
+const PORT = 3030;
+app.listen(PORT, () => {
+  console.log("Now listening on http://localhost:" + PORT);
+});
+
+app.get("/mongo", (req, res) => {
+    results = menudb.find({})
+  return res.status(200).send("results"); 
+});
+
+app.post("/mongo", (req, res) => {  
+  menudb.insertOne(req.body);
+  return res.status(201).send('Item Added')
+});
+
+app.delete("/mongo", (req,res)=>{
+    
+    menudb.deleteOne({name: req.body.name})
+    .then( () => res.send('item deleted!'))
+})
+
+app.patch("/mongo", (req,res)=>{
+    
+    menudb.updateOne({name: req.body.name}, {$set:req.body})
+    .then( () => res.send('item updated!'))
+})
